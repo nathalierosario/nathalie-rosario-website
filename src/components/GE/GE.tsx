c76724821a1bda84bb3297d07df85bc39a6850bc
nathalierosario <91161177+nathalierosario@users.noreply.github.com>
2024-04-16 17:41:42 -0400

    working

--- src/components/GE/GE.tsx
+++ src/components/GE/GE.tsx
@@ -1,10 +1,32 @@
+import Tab from "react-bootstrap/Tab";
+import Nav from "react-bootstrap/Nav";
 import { BingoProvider } from "./hooks/BingoContext";
 import BingoPage from "./pages/BingoPage";
+import HomePage from "./pages/HomePage";
 
 export default function GE() {
   return (
     <BingoProvider>
-      <BingoPage />
+      <div className="tab-div">
+        <Tab.Container defaultActiveKey="bingo">
+          <Nav>
+            <Nav.Item>
+              <Nav.Link eventKey="bingo">BINGO</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="home">HOME</Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Tab.Content>
+            <Tab.Pane eventKey="bingo">
+              <BingoPage />
+            </Tab.Pane>
+            <Tab.Pane eventKey="home">
+              <HomePage />
+            </Tab.Pane>
+          </Tab.Content>
+        </Tab.Container>
+      </div>
     </BingoProvider>
   );
 }
